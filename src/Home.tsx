54441a8679f78a0cad82316ce1d02eb9ae14553b
Ruben D. <ruben.dedecker@ugent.be>
2025-03-18 14:19:02 +0100

    update readme

--- src/Home.tsx
+++ src/Home.tsx
@@ -10,17 +10,13 @@ const Home = () => {
                 <b>Welcome to the RDF Context Associations Demo.</b>
             </Typography>        
             <Typography>
-                    This demo provides an interface both for contextualizing existing data,
-                    and an interface for processing contextualized data. Both can be found in the menu above!
+                This demo provides an interface both for contextualizing existing data,
+                and an interface for processing contextualized data. Both can be found in the menu above!
             </Typography>
             <Typography>
-                    I am currently in the process of web-izing my CLI toolchain for this, 
-                    leaving the website semi in-progress. 
-                    A full tutorial will be available here by 20.03.2025
+                The current demo is being upgraded for ESWC 2025, 
+                with the addition of hover context when hovering output RDF statements.
             </Typography>
-            <Typography>
-                github link: <a href="https://github.com:Dexagod/RDF-containment">https://github.com:Dexagod/RDF-containment</a>
-            </Typography>        
         </Box>
     )
 }
